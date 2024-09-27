48f0135a27655d5c31c1698005c16ef4ce7ae015
ruthpuna <ruthwikpunati@gmail.com>
2024-09-27 11:56:58 +0530

    added a fun endpoint

--- app/Backend/app.js
+++ app/Backend/app.js
@@ -15,6 +15,7 @@ const reviewRouter = require('./routers/reviewRouter')
 
 const userRouter = require('./routers/userRouter')
 const chatRouter = require('./routers/chatRouter')
+const { resolve } = require('path')
 
 const app = express()
 
@@ -45,6 +46,61 @@ app.use('/food', foodRouter)
 app.use('/review', reviewRouter)
 
 app.use('/chat/', chatRouter)
+
+app.use('/fun', (req, res) => {
+  setTimeout(() => {
+    res.status(200).json([
+      {
+        id: 1,
+        name: 'John Doe',
+        age: 30,
+        username: 'johndoe',
+        email: 'john.doe@example.com',
+        address: {
+          street: '123 Main Street',
+          city: 'Cityville',
+          zip: '12345',
+        },
+        phone: '555-123-4567',
+        website: 'www.johndoe.com',
+        occupation: 'Software Engineer',
+        hobbies: ['Reading', 'Gaming'],
+      },
+      {
+        id: 2,
+        name: 'Jane Smith',
+        age: 25,
+        username: 'janesmith',
+        email: 'jane.smith@example.com',
+        address: {
+          street: '456 Park Avenue',
+          city: 'Townsville',
+          zip: '54321',
+        },
+        phone: '555-987-6543',
+        website: 'www.janesmith.com',
+        occupation: 'Graphic Designer',
+        hobbies: ['Painting', 'Traveling'],
+      },
+      {
+        id: 3,
+        name: 'Michael Johnson',
+        age: 40,
+        username: 'michaeljohnson',
+        email: 'michael.johnson@example.com',
+        address: {
+          street: '789 Oak Lane',
+          city: 'Villageland',
+          zip: '67890',
+        },
+        phone: '555-456-7890',
+        website: 'www.michaeljohnson.com',
+        occupation: 'Teacher',
+        hobbies: ['Sports', 'Writing'],
+      },
+    ])
+  }, 10000)
+})
 
 app.all('*', (req, res, next) => {
   res.status(404).json({ status: 'fail', message: 'This route is not defined' })
